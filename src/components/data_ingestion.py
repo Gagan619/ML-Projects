dbeb37f10a7f666e4fa867d7927be88588463a40
Gagandeep Saini <gagans1202@gmail.com>
2025-07-09 23:21:34 +0530

    logging and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,1 @@
+## Data ingestion means data reading
---
+++ src/logger.py
@@ -0,0 +1,24 @@
+## Logger is for the purpose that if any execution that probably happens we should able to 
+# log all those information the execution everything in some files so that will able to track
+# if there is some errors even the custom exception error try to log that into the text file 
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
+
+
+
