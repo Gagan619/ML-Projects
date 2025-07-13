04e5901083cf721a332134328bc53507a02ed12a
Gagandeep Saini <gagans1202@gmail.com>
2025-07-13 21:38:28 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,15 @@
 # Data ingestion means data reading
 import os
 import sys
-from src.exception import CustoException
+from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 # Define Data ingestion path
 @dataclass
@@ -53,9 +56,12 @@ class DataIngestion:
 
 
         except Exception as e :
-            raise CustoException(e,sys)
+            raise CustomException(e,sys)
         
 
 if __name__ == "__main__" :
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
